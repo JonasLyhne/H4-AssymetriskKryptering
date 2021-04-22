6d4dffe4655369459e375f1f5f6eec98174caf7e
Jonas Iversen <Joi>
2021-04-22 14:25:28 +0200

    Initial commit

---
+++ AsymmetriskKryptering.RsaEncryption/Encryption/RsaEncryptor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AsymmetriskKryptering.RsaEncryption.Encryption
+{
+    public class RsaEncryptor
+    {
+        private const string Container = "PrivateContainer";
+
+        public string Modolus { get; private set; }
+
+        public string Exponent { get; private set; }
+
+        public RsaEncryptor()
+        {
+            this.CreateKey();
+        }
+
+        public CspParameters AssignKey()
+        {
+            CspParameters cspParameters = new CspParameters(1);
+            cspParameters.KeyContainerName = Container;
+            cspParameters.Flags = CspProviderFlags.UseMachineKeyStore;
+            cspParameters.ProviderName = "Microsoft Strong Cryptographic Provider";
+            return cspParameters;
+        }
+
+        public void DeleteKey()
+        {
+            var csp = new CspParameters { KeyContainerName = Container };
+        }
+
+        public byte[] Encrypt(byte[] inputToEncrypt)
+        {
+            var csp = new CspParameters { KeyContainerName = Container };
+
+            using var rsa = new RSACryptoServiceProvider(2048, csp);
+            return rsa.Encrypt(inputToEncrypt, true);
+        }
+
+        public byte[] Decrypt(byte[] inputToDecrypt)
+        {
+            var csp = new CspParameters { KeyContainerName = Container };
+
+            using var rsa = new RSACryptoServiceProvider(2048, csp);
+            return rsa.Decrypt(inputToDecrypt, true);
+        }
+
+        private void CreateKey()
+        {
+            var csp = new CspParameters { KeyContainerName = Container };
+            using var rsa = new RSACryptoServiceProvider(2048, csp);
+            var myParam = rsa.ExportParameters(true);
+            Modolus = BitConverter.ToString(myParam.Modulus);
+            Exponent = BitConverter.ToString(myParam.Exponent);
+        }
+    }
+}
---
+++ AsymmetriskKryptering.Sender.WPF/ViewModel/RsaSenderVIewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using AsymmetriskKryptering.RsaEncryption.Encryption;
+
+namespace AsymmetriskKryptering.Sender.WPF.ViewModel
+{
+    public class RsaSenderVIewModel : INotifyPropertyChanged
+    {
+        private RsaEncryptor rsaEncryptor; 
+        private ICommand encrypt;
+        private string exponent;
+        private string messageToEncrypt;
+        private string encryptedMessageAsString;
+
+        private string modolus;
+
+        public string Modolus
+        {
+            get { return modolus; }
+            set { modolus = value; OnPropertyChanged(nameof(Modolus)); }
+        }
+
+
+        public string EncryptedMessageAsString
+        {
+            get { return encryptedMessageAsString; }
+            set { encryptedMessageAsString = value; OnPropertyChanged(nameof(EncryptedMessageAsString)); }
+        }
+
+
+        public string MessageToEncrypt
+        {
+            get { return messageToEncrypt; }
+            set { messageToEncrypt = value; OnPropertyChanged(nameof(MessageToEncrypt)); }
+        }
+
+
+        public string Exponent
+        {
+            get { return exponent; }
+            set { exponent = value; }
+        }
+
+
+        public ICommand Encrypt
+        {
+            get { return encrypt; }
+            set { encrypt = value; }
+        }
+
+        public RsaSenderVIewModel()
+        {
+            Encrypt = new DelegateCommand(EncryptMessage);
+            rsaEncryptor = new RsaEncryptor();
+            Modolus = rsaEncryptor.Modolus;
+            Exponent = rsaEncryptor.Exponent;
+        }
+
+        private void EncryptMessage(object obj)
+        {
+            var param = rsaEncryptor.AssignKey();
+            var encryptedMessage = rsaEncryptor.Encrypt(Encoding.UTF8.GetBytes(messageToEncrypt));
+            EncryptedMessageAsString = BitConverter.ToString(encryptedMessage);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
